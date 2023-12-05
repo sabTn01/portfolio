e67b15c88128c81b6301124e56d79e173638c59a
sabtn01 <sbh.myproject@gmail.com>
2023-12-05 12:33:10 -0500

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
     <div className="footer bg-[#171717]">
       <div className="wrap flex justify-between items-center px-12 text-white font-bold py-6 tl:px-4 tl:py-4 ">
         <div className="flex gap-4 cursor-pointer ">
-          <a href="https://twitter.com/reayhs" target="_blank">
+          <a href="https://twitter.com/Bitcoin" target="_blank">
             <i className="fa-brands fa-twitter border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
           </a>
-          <a href="https://www.instagram.com/iburakz" target="_blank">
+          <a href="https://www.instagram.com/bitcoinmagazine/" target="_blank">
             <i className="fa-brands fa-instagram border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
           </a>
           <a href="https://github.com/sabTn01" target="_blank">
